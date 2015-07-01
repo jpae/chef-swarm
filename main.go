df11ecf2e2de43efe0803e4fe5dd61d089302de7
Paul Mooring <paul@chef.io>
2015-07-01 15:08:02 -0700

    Fixed multi-org support

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	config "github.com/paulmooring/chef-swarm/lib/config"
 	org "github.com/paulmooring/chef-swarm/lib/org"
 	"os"
+	"sync"
 )
 
 func main() {
@@ -14,25 +15,26 @@ func main() {
 		os.Exit(1)
 	}
 
-	var org_name string
-	var org_conf config.OrgConfig
+	var wait_group sync.WaitGroup
+	wait_group.Add(len(cfg.Organizations))
 
 	for name, conf := range cfg.Organizations {
-		org_name = name
-		org_conf = conf
+		go func(org_name string, org_conf config.OrgConfig) {
+			defer wait_group.Done()
 
-		break
+			server_string := "https://" + cfg.Server + "/organizations/" + org_name
+
+			o, err := org.NewOrg(server_string, org_conf)
+			if err != nil {
+				fmt.Println(err.Error())
+			}
+
+			err = o.ChefRuns()
+			if err != nil {
+				fmt.Println(err.Error())
+			}
+		}(name, conf)
 	}
 
-	server_string := "https://" + cfg.Server + "/organizations/" + org_name
-
-	o, err := org.NewOrg(server_string, org_conf)
-	if err != nil {
-		fmt.Println(err.Error())
-	}
-
-	err = o.ChefRuns()
-	if err != nil {
-		fmt.Println(err.Error())
-	}
+	wait_group.Wait()
 }
